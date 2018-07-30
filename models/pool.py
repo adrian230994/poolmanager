c7df5325d22ae24ed3c669930789c03c5d93cbc5
Adrián <adrian@deletos.es>
2018-07-30 23:07:35 +0200

    Deleted comments of the code

--- models/pool.py
+++ models/pool.py
@@ -3,16 +3,5 @@ class Pool:
     players = {}
     number_balls = 15
 
-    def __init__(self):
-        pass
-
     def __str__(self):
         return " - ".join(["%s (%s)" % (name, player.points) for (name, player) in self.players.items()])
-        '''result = ""
-        sep = " - "
-        lista = []
-        # return "jugadores %s" % self.players
-        for (name, player) in self.players.items():
-            result += player.name + player.points + sep
-
-        return result[:-len(sep)]'''
